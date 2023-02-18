b275f3de4db072987fb9ad140afbc73ad3f9bb6d
Xemorph <finn.brandes@outlook.de>
2023-02-18 21:29:31 +0100

    [+](Add) Added error callback & 'verify' functionality

--- include/eebl0/eebl0.h
+++ include/eebl0/eebl0.h
@@ -10,9 +10,27 @@ extern "C" {
 #include <stdint.h>
 #include <stdbool.h>
 
+enum
+{
+    EEBL0_ERROR_CBLOG_SUCCESS             = 0  ,
+    EEBL0_ERROR_CBLOG_STDLIB_MALLOC_FAIL  = 1  ,
+    EEBL0_ERROR_CBLOG_EF_NULL_INPUT       = 2  ,
+    EEBL0_ERROR_CBLOG_ESP_NULL_INPUT      = 3  ,
+    EEBL0_ERROR_CBLOG_ESP_NULL_INPUT      = 4  ,
+    EEBL0_ERROR_CBLOG_EC_NULL_INPUT       = 5  ,
+    EEBL0_ERROR_CBLOG_EC_RS232OPEN_FAIL   = 6  ,
+    EEBL0_ERROR_CBLOG_EDC_NULL_INPUT      = 7  ,
+    EEBL0_ERROR_CBLOG_EE_NULL_INPUT       = 8  ,
+    EEBL0_ERROR_CBLOG_EE_RS232SEND_FAIL   = 9  ,
+    EEBL0_ERROR_CBLOG_EE_RS232READ_FAIL   = 10 ,
+    EEBL0_ERROR_CBLOG_EV_NULL_INPUT       = 11 ,
+    EEBL0_ERROR_CBLOG_EV_RS232SEND_FAIL   = 12 ,
+    EEBL0_ERROR_CBLOG_EV_RS232READ_FAIL   = 13 ,
+};
+
 struct EEBL0;
 
-struct EEBL0 * EEBL0_create(void);
+struct EEBL0 * EEBL0_create(void (*cb_log)(int code));
 void EEBL0_free(struct EEBL0 ** self);
 
 void EEBL0_setPort(struct EEBL0 * self, const char * const port);
@@ -24,6 +42,7 @@ const char * const EEBL0_getStatus(struct EEBL0 * self);
 bool EEBL0_connect(struct EEBL0 * self);
 void EEBL0_disconnect(struct EEBL0 * self);
 bool EEBL0_erase(struct EEBL0 * self);
+bool EEBL0_verfiy(struct EEBL0 * self, uint8_t * data, size_t data_sz);
 
 #ifdef __cplusplus
 }
--- src/eebl0.c
+++ src/eebl0.c
@@ -3,6 +3,13 @@
 #include <string.h>
 #include <rs232/rs_232.h>
 
+
+#define LOG_ERROR_IF_FOUND(x) \
+    do \
+    { \
+        if (eebl0->cb_log != NULL) eebl0->cb_log((x)); \
+    } while (0)
+
 static const char * const _WELCOME = "EEBL0";
 static const char * const _READABLE_STATUS[] = {
     "CONNECTING", "CONNECTED", "DISCONNECTING", "DISCONNECTED", "FAILURE"
@@ -12,6 +19,7 @@ struct EEBL0 {
     char * port;
     int    port_id;
     int    status;
+    void (*cb_log)(int code);
 };
 
 enum EEBL0_UART {
@@ -31,22 +39,31 @@ enum EEBL0_STATUS {
     EEBL0_STATUS_FAILURE = 4
 };
 
-struct EEBL0 * EEBL0_create(void) {
+struct EEBL0 * EEBL0_create(void (*cb_log)(int code)) {
     struct EEBL0 * eebl0;
 
     eebl0 = malloc(sizeof(*eebl0));
     if (!eebl0) {
+        if (cb_log != NULL) cb_log(EEBL0_ERROR_CBLOG_STDLIB_MALLOC_FAIL);
         return (NULL);
     }
 
     memset(eebl0, 0, sizeof(*eebl0));
     eebl0->port_id = INT_MIN;
     eebl0->status = EEBL0_STATUS_DISCONNECTED;
+    // Hook the logging mechanism if available
+    if (cb_log != NULL) eebl0->cb_log = cb_log;
+    else eebl0->cb_log = NULL;
 
     return (eebl0);
 }
 
 void EEBL0_free(struct EEBL0 ** self) {
+    if (self == NULL || *self == NULL) {
+        LOG_ERROR_IF_FOUND(EEBL0_ERROR_CBLOG_EF_NULL_INPUT);
+        return;
+    }
+
     EEBL0_disconnect(*self); // Always call disconnect
     if ((*self)->port) {
         free((*self)->port);
@@ -66,6 +83,11 @@ const char * const EEBL0_getStatus(struct EEBL0 * self) {
 }
 
 void EEBL0_setPort(struct EEBL0 * self, const char * const port) {
+    if (self == NULL || port == NULL) {
+        LOG_ERROR_IF_FOUND(EEBL0_ERROR_CBLOG_ESP_NULL_INPUT);
+        return;
+    }
+
     size_t sz = (sizeof(*port) * strlen(port)) + 1;
     self->port = malloc(sz);
     if (self->port) {
@@ -74,10 +96,20 @@ void EEBL0_setPort(struct EEBL0 * self, const char * const port) {
     }
 }
 const char * const EEBL0_getPort(struct EEBL0 * self) {
+    if (self == NULL) {
+        LOG_ERROR_IF_FOUND(EEBL0_ERROR_CBLOG_EGP_NULL_INPUT);
+        return;
+    }
+
     return((const char * const)self->port);
 }
 
 bool EEBL0_connect(struct EEBL0 * self) {
+    if (self == NULL) {
+        LOG_ERROR_IF_FOUND(EEBL0_ERROR_CBLOG_EC_NULL_INPUT);
+        return;
+    }
+
         self->status  = EEBL0_STATUS_CONNECTING;
         self->port_id = RS232_GetPortnr(self->port);
            int bdrate = 115200; // Default
@@ -90,6 +122,7 @@ bool EEBL0_connect(struct EEBL0 * self) {
 
     if (RS232_OpenComport(self->port_id, bdrate, &mode[0], 0) != 0) {
         self->status = EEBL0_STATUS_FAILURE;
+        LOG_ERROR_IF_FOUND(EEBL0_ERROR_CBLOG_EC_RS232OPEN_FAIL);
         return (false);
     }
 
@@ -98,6 +131,11 @@ bool EEBL0_connect(struct EEBL0 * self) {
 }
 
 void EEBL0_disconnect(struct EEBL0 * self) {
+    if (self == NULL) {
+        LOG_ERROR_IF_FOUND(EEBL0_ERROR_CBLOG_EDC_NULL_INPUT);
+        return;
+    }
+
     if (self->port_id != INT_MIN) {
         self->status = EEBL0_STATUS_DISCONNECTING;
         RS232_CloseComport(self->port_id);
@@ -107,6 +145,11 @@ void EEBL0_disconnect(struct EEBL0 * self) {
 }
 
 bool EEBL0_erase(struct EEBL0 * self) {
+    if (self == NULL) {
+        LOG_ERROR_IF_FOUND(EEBL0_ERROR_CBLOG_EE_NULL_INPUT);
+        return;
+    }
+
     if (self->status != EEBL0_STATUS_CONNECTED) {
         return (false);
     }
@@ -114,9 +157,62 @@ bool EEBL0_erase(struct EEBL0 * self) {
     uint8_t rx;
     int bWritten = RS232_SendByte(self->port_id, EEBL0_UART_CMD_ERASE);
     if (bWritten == -1 || bWritten == 0) {
+        LOG_ERROR_IF_FOUND(EEBL0_ERROR_CBLOG_EE_RS232SEND_FAIL);
         return (false);
     }
 
-    RS232_PollComport(self->port_id, &rx, 1);
+    int bRead = RS232_PollComport(self->port_id, &rx, 1);
+    if (bRead == -1) {
+        LOG_ERROR_IF_FOUND(EEBL0_ERROR_CBLOG_EE_RS232READ_FAIL);
+        return (false);
+    }
+
     return (rx == (uint8_t)'s');
+}
+
+bool EEBL0_verfiy(struct EEBL0 * self, const uint8_t * const data, size_t data_sz) {
+    if (self == NULL || data == NULL || data_sz <= 0) {
+        LOG_ERROR_IF_FOUND(EEBL0_ERROR_CBLOG_EV_NULL_INPUT);
+        return (false);
+    }
+
+    // Require 16K-1K-256B sized firmware
+    uint8_t bfr[15104];
+    uint8_t rx[65];
+
+    int j;
+    for (j = data_sz; j < data_sz; j++)
+        bfr[j] = 0xFF;
+
+    memcpy(&bfr[0], data, data_sz > 15104 ? 15104 : data_sz);
+
+    // Page firmware
+    int page;
+    for (page = 0; page < 15104 / 64; page++) {
+        // Verify
+        int bWritten = RS232_SendByte(self->port_id, EEBL0_UART_CMD_VERIFY_PAGE);
+        if (bWritten == -1 || bWritten == 0) {
+            LOG_ERROR_IF_FOUND(EEBL0_ERROR_CBLOG_EV_RS232SEND_FAIL);
+            return (false);
+        }
+
+        int bRead = RS232_PollComport(self->port_id, &rx, 65);
+        if (bRead == -1) {
+            LOG_ERROR_IF_FOUND(EEBL0_ERROR_CBLOG_EV_RS232READ_FAIL);
+            return (false);
+        }
+        if (rx[0] != (uint8_t)'s') {
+            return (false);
+        }
+
+        // Compare page
+        int y;
+        for(int y = 0; y < 64; y++) {
+            if(bfr[page * 64 + y] != rx[y + 1]) {
+                return (false);
+            }
+        }
+    }
+
+    return (true);
 }
